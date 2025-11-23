3187972fb2cb68e52710d2dab0a7d4d8e60630b5
John Cook-Martin <johncookmartin@gmail.com>
2025-11-23 11:52:12 -0500

    Removing unused projects and files

--- BudgetApp.Application/Auth/IAuthService.cs
+++
@@ -1,7 +0,0 @@
-﻿using BudgetApp.Domain.Entities;
-
-namespace BudgetApp.Application.Auth;
-public interface IAuthService
-{
-    Task<BudgetUser> EnsureUserAsync(string googleSub, string email, string displayName, string? pictureUrl, string? familyName, string? givenName);
-}
