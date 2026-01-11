e91ff9811deb0c3cce702fbed77c436ef776b0cd
John Cook-Martin <johncookmartin@gmail.com>
2026-01-11 12:02:16 -0500

    Adding 3001 to dev CORS

--- BudgetApp.Api/Program.cs
+++ BudgetApp.Api/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins("http://localhost:3000")
+                .WithOrigins("http://localhost:3000", "http://localhost:3001")
                 .AllowCredentials()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
